18ad1a32bd33365cc9007a80cd49417d060d95e6
Alyona0911 <138192903+Alyona0911@users.noreply.github.com>
2023-08-31 02:12:19 +0300

    сгенерировала второй массив

--- task/Program.cs
+++ task/Program.cs
@@ -3,9 +3,18 @@
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-void SecondArray (string [] array1 )
+
+void SecondArray (string [] array1, string [] array2  )
 {
-   for( int i = 0; i < array1.Length; i++) ;
+    int count = 0 ;
+    for( int i = 0; i < array1.Length; i++) 
+    {
+        if(array1[i].Length < 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
 }
 
 
@@ -13,5 +22,5 @@ void SecondArray (string [] array1 )
 
 string [] array1 = new string[5] {"6746", "hi", "home", "world", "7685"};
 string [] array2 = new string [array1.Length] ;
-SecondArray ( array1 );
-Console.Write($"  [{String.Join(",", array1)}]");
+SecondArray ( array1, array2 );
+
