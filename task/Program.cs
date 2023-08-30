5337ca793d3cfbb523ebf53a37badfca39f57458
Alyona0911 <138192903+Alyona0911@users.noreply.github.com>
2023-08-31 01:53:40 +0300

    сгенерировала первый массив

--- task/Program.cs
+++ task/Program.cs
@@ -3,23 +3,15 @@
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-// string [] FillArray ( string [] array1  )
-// {
-//     string [] array = new string [size] ;
-//     for(int i = 0 ;i < size; i++)
-//     {
-//         array [i] = new Random().Next(start,finish+1) ;
-//     }
-//     return array1 ;
-// }
-
-int GetInput(string text)
+void SecondArray (string [] array1 )
 {
-    Console.Write(text);
-    return Convert.ToInt32(Console.ReadLine());
+   for( int i = 0; i < array1.Length; i++) ;
 }
 
-int size  = GetInput("Введите массив строк: ");
-string [] array1 = new string [size];
+
+
+
+string [] array1 = new string[5] {"6746", "hi", "home", "world", "7685"};
 string [] array2 = new string [array1.Length] ;
-// FillArray (array1) ;
+SecondArray ( array1 );
+Console.Write($"  [{String.Join(",", array1)}]");
