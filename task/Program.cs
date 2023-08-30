4013459880c28165e9eb73d083e442e615f60114
Alyona0911 <138192903+Alyona0911@users.noreply.github.com>
2023-08-31 00:42:17 +0300

    Ввод массива с клавиатуры

---
+++ task/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+//  длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// string [] FillArray ( string [] array1  )
+// {
+//     string [] array = new string [size] ;
+//     for(int i = 0 ;i < size; i++)
+//     {
+//         array [i] = new Random().Next(start,finish+1) ;
+//     }
+//     return array1 ;
+// }
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size  = GetInput("Введите массив строк: ");
+string [] array1 = new string [size];
+string [] array2 = new string [array1.Length] ;
+// FillArray (array1) ;
