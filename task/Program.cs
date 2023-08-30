ad91f585df9e555d2d68728efb41ef933d83349a
Alyona0911 <138192903+Alyona0911@users.noreply.github.com>
2023-08-31 02:15:00 +0300

    пробуем вывести на экран (принт)

--- task/Program.cs
+++ task/Program.cs
@@ -3,6 +3,14 @@
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
 void SecondArray (string [] array1, string [] array2  )
 {
@@ -18,9 +26,7 @@ void SecondArray (string [] array1, string [] array2  )
 }
 
 
-
-
 string [] array1 = new string[5] {"6746", "hi", "home", "world", "7685"};
 string [] array2 = new string [array1.Length] ;
 SecondArray ( array1, array2 );
-
+PrintArray(array2);
